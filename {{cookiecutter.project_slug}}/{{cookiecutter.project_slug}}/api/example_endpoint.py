116ff639fe56b86728910c7ba52eeb42186ccb44
Janis <janis@customprogrammingsolutions.com>
2019-09-07 22:25:49 +1000

    Add docstrings

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/api/example_endpoint.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/api/example_endpoint.py
@@ -1,4 +1,7 @@
+"""This file provides the implementation for the /example_endpoint API endpoint"""
+
 def search():
+    """This is called when someone does a GET request with no parameters to /example_endpoint"""
     data = [
         {"name": "first_key", "value": "string one"},
         {"name": "second_key", "value": "string two"},

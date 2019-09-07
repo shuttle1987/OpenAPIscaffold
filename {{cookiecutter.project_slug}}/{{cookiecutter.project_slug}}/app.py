d8f5c27adc07c9ed8642da733cbe27d339c7d9f8
Janis <janis@customprogrammingsolutions.com>
2019-09-07 19:38:31 +1000

    App creation

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app.py
@@ -0,0 +1,24 @@
+import connexion
+from flask import Flask
+
+def create_app(config_object=ProdConfig) -> Flask:
+    """An application factory, as explained here: http://flask.pocoo.org/docs/patterns/appfactories/.
+    :param config_object: The configuration object to use.
+    """
+    # Create the API endpoints from YAML specification
+    connexion_app = connexion.FlaskApp(__name__, specification_dir='.')
+    register_swagger_api(connexion_app)
+
+    # fetch underlying flask app from the connexion app
+    app = connexion_app.app
+    app.config.from_object(config_object)
+
+    return app
+
+def register_swagger_api(connexion_flask_app: connexion.FlaskApp) -> None:
+    """Take a connexion FlaskApp and register swagger API"""
+    connexion_flask_app.add_api(
+        'api_spec.yaml',
+        resolver=RestyResolver('api'),
+        validate_responses=True
+    )
